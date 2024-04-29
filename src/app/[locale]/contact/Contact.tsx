3ba8e83099906024c8b35115238b117d97e42a84
nesta.web <nesta.web.dev@gmail.com>
2024-04-29 18:12:02 +0200

    form

--- src/app/[locale]/contact/Contact.tsx
+++ src/app/[locale]/contact/Contact.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import { NavBar, HeroHeader, Card, Bottom, Footer, HubSpotForm } from '@/components/components';
 import {useTranslations} from 'next-intl';
 import {unstable_setRequestLocale} from 'next-intl/server';
-import ContactForm from './ContactForm';
 // import { HubspotProvider } from '@aaronhayes/react-use-hubspot-form';
 // import { HubspotProvider } from 'next-hubspot';
 
@@ -52,40 +51,7 @@ const Contact: React.FC<IContact> = ({ locale }) => {
 				</div>
 			</div>
 			<div className='w-[90vw] lg:w-3/6 mx-auto -translate-y-[17.5vh] z-20 relative'>
-				{/* <HubspotProvider> */}
-					<HubSpotForm locale={locale}  />
-				{/* </HubspotProvider> */}
-				
-				{/* <form action="#" className="space-y-8 z-10 relative">
-					<div className='flex gap-4'>
-						<div className='w-1/2'>
-							<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 relative z-10">First Name</label>
-							<input type="text" id="firstName" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light relative z-10" placeholder="John" required />
-						</div>
-						<div className='w-1/2'>
-							<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 relative z-10">Last Name</label>
-							<input type="text" id="lastName" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light relative z-10" placeholder="Doe" required />
-						</div>
-					</div>
-					<div>
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 relative z-10">Your email</label>
-						<input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light relative z-10" placeholder="name@revolusapce.com" required />
-					</div>
-					<div>
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 relative z-10">Company Name</label>
-						<input type="text" id="companyName" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light relative z-10" placeholder="Revoluspace" required />
-					</div>
-					<div>
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-						<input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light relative z-10" placeholder="Let us know how we can help you" required />
-					</div>
-					<div className="sm:col-span-2">
-						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 relative z-10">Your message</label>
-						<textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-brand-white dark:focus:ring-primary-500 dark:focus:border-primary-500 relative z-10" placeholder="Leave a comment..."></textarea>
-					</div>
-					
-					<button type="submit" className="py-3 px-5 text-sm font-medium text-center text-brand-white rounded-lg bg-brand-violet-700 sm:w-fit hover:bg-brand-violet-700/80 focus:ring-4 focus:outline-none dark:bg-brand-violet-700/80 dark:hover:bg-brand-violet-700/60 ease-in-out duration-200">Send message</button>
-				</form> */}
+				<HubSpotForm locale={locale}  />
 			</div>
 			<Bottom 
 				title={t('home.getstarted.title')}
--- src/components/HubspotForm.tsx
+++ src/components/HubspotForm.tsx
@@ -8,8 +8,8 @@ export default function HubSpotForm({locale}: {locale: string}){
         document.body.appendChild(script);
 
         script.addEventListener('load', () => {
-            if(window.hbspt){
-                window.hbspt.forms.create({
+            if(window && (window as any).hbspt){
+                (window as any).hbspt.forms.create({
                     portalId: '25331240',
                     formId: locale == 'en' ? "b994f17b-5539-4642-af26-42e025cd2c86" : "aa99fa65-8b56-4d64-94e6-993a3f44eeb0",
                     target: '#hubspotForm'
